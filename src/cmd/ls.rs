365b89d0dc5c60cc827ffa7c69d2a0957588f3cf
Eugene Kalinin <e.v.kalinin@gmail.com>
2017-01-12 20:21:23 +0300

    ls: added help output

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -1,5 +1,21 @@
 use common::{EnvHome};
 
-pub fn run(e: &EnvHome, _: &[String]) -> () {
+static DESC: &'static str = "List environments";
+
+static HELP: &'static str = "
+Options:
+  --no-meta    Do not show meta information of the environment
+";
+
+pub fn run(e: &EnvHome, argv: &[String]) -> () {
+    if argv.len() > 0 && argv[0] == "--help" {
+        usage();
+        return;
+    }
     e.print_environments()
+}
+
+pub fn usage() {
+    println!("{}", DESC);
+    println!("{}", HELP);
 }
