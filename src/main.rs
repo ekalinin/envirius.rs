ee35b69ad25be2f7ae16590f072a6772292d69ed
Eugene Kalinin <e.v.kalinin@gmail.com>
2017-01-18 20:37:56 +0300

    first try of the clap

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate clap;
+
 use std::env;
+use clap::{App, Arg, SubCommand, AppSettings};
 
 mod common;
 mod cmd;
+
 
 
 /*
@@ -22,6 +27,24 @@ fn main() {
     let nv = common::Nv::new(nv_home);
 
     // cli args
+    let app = App::new("enviriusx")
+        .setting(AppSettings::SubcommandRequired)
+        .about("Universal Virtual Environment Manager")
+        .version(crate_version!())
+        .version_short("v");
+
+    let m = app.subcommand(
+            SubCommand::with_name("ls")
+                .about("List environments")
+                .arg(
+                    Arg::with_name("no-meta")
+                ))
+        .get_matches();
+
     let args: Vec<_> = env::args().collect();
-    cmd::run(args[1].as_ref(), &nv, &args[2..]);
+    // cmd::run(args[1].as_ref(), &nv, &args[2..]);
+    match m.subcommand_name() {
+        Some(cmd)   => cmd::run(cmd, &nv, &args[2..]),
+        None        => println!("Wrong command"),
+    }
 }
