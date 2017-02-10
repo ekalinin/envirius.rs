fb3ee4ba01721f2624c835fa88cdd0c8358f7a15
Eugene Kalinin <e.v.kalinin@gmail.com>
2017-02-10 13:30:09 +0300

    Improved mk command

--- src/cmd/mk.rs
+++ src/cmd/mk.rs
@@ -5,11 +5,11 @@ use clap::{App, Arg, SubCommand, ArgMatches};
 
 
 pub fn run(e: &Nv, args: &ArgMatches) -> super::CmdResult {
-    println!("Create environment ...");
     println!("  On:     {}", args.is_present("on"));
     println!("  Force:  {}", args.is_present("force"));
     println!("  Name:   {}", args.value_of("name").unwrap_or("undefined"));
-    println!("  Plugins:{:?}", args.values_of("plugins").unwrap().collect::<Vec<_>>());
+    println!("  Plugins:{:?}",
+             args.values_of("plugins").unwrap().collect::<Vec<_>>());
     for p in args.values_of("plugins").unwrap().collect::<Vec<_>>() {
         println!(" * {:?}", Lang::from(p));
     }
@@ -26,10 +26,14 @@ pub fn run(e: &Nv, args: &ArgMatches) -> super::CmdResult {
         }
     } else {
         let langs = args.values_of("plugins")
-            .unwrap().collect::<Vec<_>>().iter()
+            .unwrap()
+            .collect::<Vec<_>>()
+            .iter()
             .map(|p| Lang::from(p))
             .collect::<Vec<_>>();
-        e.create_env(env_name, langs);
+        if !e.create_env(env_name, langs) {
+            return Err(2);
+        }
     }
 
     Ok(())
@@ -38,26 +42,17 @@ pub fn run(e: &Nv, args: &ArgMatches) -> super::CmdResult {
 pub fn get_command<'a>() -> App<'a, 'a> {
     SubCommand::with_name("mk")
         .about("Create environment")
-        .arg(
-            Arg::with_name("on")
-                .long("on")
-                .help("Activate environment after installation")
-        )
-        .arg(
-            Arg::with_name("force")
-                .long("force")
-                .help("Re-create environment if it already exists")
-        )
-        .arg(
-            Arg::with_name("name")
-                .help("Environment name")
-                .required(true)
-        )
-        .arg(
-            Arg::with_name("plugins")
-                .help("List of plugins needed in the environment (example: go=1.7.3)")
-                .multiple(true)
-                .required(true)
-        )
+        .arg(Arg::with_name("on")
+            .long("on")
+            .help("Activate environment after installation"))
+        .arg(Arg::with_name("force")
+            .long("force")
+            .help("Re-create environment if it already exists"))
+        .arg(Arg::with_name("name")
+            .help("Environment name")
+            .required(true))
+        .arg(Arg::with_name("plugins")
+            .help("List of plugins needed in the environment (example: go=1.7.3)")
+            .multiple(true)
+            .required(true))
 }
-
